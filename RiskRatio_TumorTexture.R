aa297c32a73d7dc760af2ac38c6fa5eb8d66b4c4
YOUR NAME <YOUR@email.com>
2021-10-05 12:42:50 -0400

    renaming variable

--- RiskRatio_TumorTexture.R
+++ RiskRatio_TumorTexture.R
@@ -16,12 +16,12 @@ Cm_data <- data %>% filter(species == "Chelonia mydas") %>% # filter to just Cm
   filter(FP == 1) # filter to just FP + Cms, 106 total
 
 
-# Subset to the alleles that occur in 10 or more greens with FP and the columns of sample_ID and paps_texture (0s and 1s) but include Chmy33 (only occurs in 4 C. mydas with FP), since it had very high relative importance in the random forest model.
-Cm_data <- subset(Cm_data, select = c(sample_ID, paps_texture, Chmy02, Chmy01, Chmy10, Chmy04, Chmy05, Chmy33))
+# Subset to the alleles that occur in 10 or more greens with FP and the columns of sample_ID and paps_smooth_regressed (0s and 1s) but include Chmy33 (only occurs in 4 C. mydas with FP), since it had very high relative importance in the random forest model.
+Cm_data <- subset(Cm_data, select = c(sample_ID, paps_smooth_regressed, Chmy02, Chmy01, Chmy10, Chmy04, Chmy05, Chmy33))
 
 
-Cm_data[Cm_data$paps_texture == 1, "paps_texture"] <- "smooth" # replace 1s with smooth texture
-Cm_data[Cm_data$paps_texture == 0, "paps_texture"] <- "rough" # replace 0s with rough texture
+Cm_data[Cm_data$paps_smooth_regressed == 1, "paps_smooth_regressed"] <- "smooth" # replace 1s with smooth texture
+Cm_data[Cm_data$paps_smooth_regressed == 0, "paps_smooth_regressed"] <- "rough" # replace 0s with rough texture
 
 attach(Cm_data)
 
@@ -29,7 +29,7 @@ attach(Cm_data)
 Cm_data[Cm_data$Chmy01 == 1, "Chmy01"] <- "Chmy01 +"
 Cm_data[Cm_data$Chmy01 == 0, "Chmy01"] <- "Chmy01 -"
 
-tab_Chmy01 <- table(Chmy01, paps_texture) # independent, dependent
+tab_Chmy01 <- table(Chmy01, paps_smooth_regressed) # independent, dependent
 tab_Chmy01
 
 # rearrange so it's in ABCD format
@@ -53,7 +53,7 @@ df_Chmy01 <- as.data.frame(Chmy01_risk$massoc.summary)
 Cm_data[Cm_data$Chmy02 == 1, "Chmy02"] <- "Chmy02 +"
 Cm_data[Cm_data$Chmy02 == 0, "Chmy02"] <- "Chmy02 -"
 
-tab_Chmy02 <- table(Chmy02, paps_texture) # independent, dependent
+tab_Chmy02 <- table(Chmy02, paps_smooth_regressed) # independent, dependent
 tab_Chmy02
 
 # rearrange so it's in ABCD format
@@ -79,7 +79,7 @@ Cm_risks <- rbind(df_Chmy01, df_Chmy02)
 Cm_data[Cm_data$Chmy04 == 1, "Chmy04"] <- "Chmy04 +"
 Cm_data[Cm_data$Chmy04 == 0, "Chmy04"] <- "Chmy04 -"
 
-tab_Chmy04 <- table(Chmy04, paps_texture) # independent, dependent
+tab_Chmy04 <- table(Chmy04, paps_smooth_regressed) # independent, dependent
 tab_Chmy04
 
 # rearrange so it's in ABCD format
@@ -105,7 +105,7 @@ Cm_risks <- rbind(Cm_risks, df_Chmy04)
 Cm_data[Cm_data$Chmy05 == 1, "Chmy05"] <- "Chmy05 +"
 Cm_data[Cm_data$Chmy05 == 0, "Chmy05"] <- "Chmy05 -"
 
-tab_Chmy05 <- table(Chmy05, paps_texture) # independent, dependent
+tab_Chmy05 <- table(Chmy05, paps_smooth_regressed) # independent, dependent
 tab_Chmy05
 
 # rearrange so it's in ABCD format
@@ -131,7 +131,7 @@ Cm_risks <- rbind(Cm_risks, df_Chmy05)
 Cm_data[Cm_data$Chmy10 == 1, "Chmy10"] <- "Chmy10 +"
 Cm_data[Cm_data$Chmy10 == 0, "Chmy10"] <- "Chmy10 -"
 
-tab_Chmy10 <- table(Chmy10, paps_texture) # independent, dependent
+tab_Chmy10 <- table(Chmy10, paps_smooth_regressed) # independent, dependent
 tab_Chmy10
 
 # rearrange so it's in ABCD format
@@ -157,7 +157,7 @@ Cm_risks <- rbind(Cm_risks, df_Chmy10)
 Cm_data[Cm_data$Chmy33 == 1, "Chmy33"] <- "Chmy33 +"
 Cm_data[Cm_data$Chmy33 == 0, "Chmy33"] <- "Chmy33 -"
 
-tab_Chmy33 <- table(Chmy33, paps_texture) # independent, dependent
+tab_Chmy33 <- table(Chmy33, paps_smooth_regressed) # independent, dependent
 tab_Chmy33
 
 # rearrange so it's in ABCD format
@@ -201,5 +201,5 @@ ggplot(data=Cm_risks, aes(x=allele, y=est, ymin=lower, ymax=upper)) +
   geom_pointrange(shape = 18) +
   geom_hline(yintercept=1, lty=2, color = "red") +  # add a dotted line at x=1 after flip
   coord_flip() +  # flip coordinates (puts labels on y axis)
-  xlab("allele") + ylab("relative risk estimate of FP texture (95% CI)") +
+  xlab("allele") + ylab("relative risk estimate of FP regression/smooth texture (95% CI)") +
   theme_bw() # use a white background
