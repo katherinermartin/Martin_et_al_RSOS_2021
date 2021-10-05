f3409ab51b8acfe3836985efc610af63f681fa3f
YOUR NAME <YOUR@email.com>
2021-10-05 12:44:12 -0400

    renamed variable, re-ran with final dataset.

--- RandomForest_TumorTexture.R
+++ RandomForest_TumorTexture.R
@@ -5,11 +5,11 @@ library(caret)
 library(dplyr)
 
 # read in data
-data_full <- read.csv("/classI_juveniles_morpho_FP_v3.csv") # this includes C. mydas and C. caretta records
+data_full <- read.csv("classI_juveniles_morpho_FP_v3.csv") # this includes C. mydas and C. caretta records
 
 data_full <- data_full %>% filter(!is.na(Carap_L_sl)) # remove any record without straight carapace length (SCL) measurement (just 1)
 
-# variables: sample ID, location, spp, year, FP, paps_texture, season, allele count, all alleles, supertypes A, B, C, and SCL ("Carap_L_sl" from database download)
+# variables: sample ID, location, spp, year, FP, paps_smooth_regressed, season, allele count, all alleles, supertypes A, B, C, and SCL ("Carap_L_sl" from database download)
 data_full <- data_full[c(1, 6, 7, 11, 14, 16, 19:139, 142)] 
 colnames(data_full)
 
@@ -18,13 +18,13 @@ data_full <- data_full %>% mutate(location = tolower(location))
 # Convert sample ID to character
 data_full$sample_ID = as.character(data_full$sample_ID)
 
-# Convert location, species, FP, paps_texture, season, and all alleles, all supertypes to factors
+# Convert location, species, FP, paps_smooth_regressed, season, and all alleles, all supertypes to factors
 col_names <- names(data_full)
 col_names
 
 data_full[col_names[2:3]] <- lapply(data_full[col_names[2:3]], factor) # location and species
 
-data_full[col_names[5:7]] <- lapply(data_full[col_names[5:7]], factor) # FP,paps_texture, season
+data_full[col_names[5:7]] <- lapply(data_full[col_names[5:7]], factor) # FP,paps_smooth_regressed, season
 
 data_full[col_names[9:127]] <- lapply(data_full[col_names[9:127]], factor) # alleles and supertypes
 
@@ -46,14 +46,14 @@ data <- data[,-5] # remove "FP" variable which is part of a separate model ("Ran
 data <- subset(data, select = -c(Caca01,Carca09,Carca32,Carca15,Carca19,Caca02,Carca14,Carca11,Carca16,Carca17,Carca27,Carca13,Chmy26,Chmy27,Carca56,Caca03,Carca28,Carca109,Caca04,Chmy59,Caca05,Carca25,Carca81,Caca06,Chmy77,Chmy78,Caca07,Chmy86,Caca08,Chmy88,Chmy92))
 
 # Check for imbalance in response variable (regression)
-length(which(data$paps_texture==0)) # 80 C. mydas with rough tumor texture
-length(which(data$paps_texture==1)) # 26 C. mydas with smooth tumor texture for 24.5% regression.
+length(which(data$paps_smooth_regressed==0)) # 77 C. mydas with rough tumor texture
+length(which(data$paps_smooth_regressed==1)) # 29 C. mydas with smooth/regressed tumor texture for 28.3% regression.
 
-# This is unbalanced enough that we need to account for it by over-sampling the under-represented class (regression) and under-sampling the over-represented class (active tumor). Set the sampsize parameter to 2/3 of the under-represented class, and use that sample size in conjunction with the strata option when building the random trees and when training.
+# This is unbalanced enough that we need to account for it by over-sampling the under-represented class (smooth/regression) and under-sampling the over-represented class (rough tumor). Set the sampsize parameter to 2/3 of the under-represented class, and use that sample size in conjunction with the strata option when building the random trees and when training.
 
-# there are 28 regressed tumor individuals; 26*(2/3) = 17.3 == 17 individuals, so we'll sample 17 smooth tumor individuals and 17 rough tumor individuals for the training data set so that the resutling tree forest is not biased.
+# there are 30 smooth/regressed tumor individuals; 29*(2/3) = 19.3 individuals, so we'll sample 19 smooth/regressed tumor individuals and 19 rough tumor individuals for the training data set so that the resutling tree forest is not biased.
 
-sample_size <- c(17,17) # will use subsequently with strata.
+sample_size <- c(19,19) # will use subsequently with strata.
 
 # Random Forest analysis
 
@@ -67,7 +67,7 @@ results_optimization <- matrix(data=NA , nrow = 0, ncol = 3) # create matrix tha
 for (i in seq(from = 100, to = 1000 , by = 100)){  # values of ntree
   print(i)
   for (j in c(9.7, 19.4, 9.4, 18.8, 31.3, 94)){    #values of mtry based on 94 total predictors
-    rf_ij <- randomForest(x = data[,2:96], y = as.factor(data$paps_texture), importance=TRUE ,proximity=TRUE, ntree=i, mtry=j, strata=as.factor(data$paps_texture), sampsize=sample_size)
+    rf_ij <- randomForest(x = data[,2:96], y = as.factor(data$paps_smooth_regressed), importance=TRUE ,proximity=TRUE, ntree=i, mtry=j, strata=as.factor(data$paps_smooth_regressed), sampsize=sample_size)
     results_optimization <- rbind(results_optimization, c(i,j,tail(rf_ij$err.rate,1)[1]))
   }
 }
@@ -94,27 +94,27 @@ metric <- "Accuracy"
 set.seed(seed)
 mtry <- sqrt(94)
 tunegrid <- expand.grid(.mtry=mtry)
-rf_default <- train(paps_texture~., data=data[,2:96], method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
-print(rf_default) # mtry = 9.695 at 75.47% accuracy.
+rf_default <- train(paps_smooth_regressed~., data=data[,2:96], method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
+print(rf_default) # mtry = 9.695 at 70.75472% accuracy.
 
 # now let's fine tune
 ## Grid search, where method = "oob"
 control <- trainControl(method="oob", number=10, search="grid")
 set.seed(seed)
 tunegrid <- expand.grid(.mtry=c(1:94)) # doing full 94
-rf_gridsearch <- train(paps_texture~., data=data[,2:96], method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
+rf_gridsearch <- train(paps_smooth_regressed~., data=data[,2:96], method="rf", metric=metric, tuneGrid=tunegrid, trControl=control)
 print(rf_gridsearch)
-plot(rf_gridsearch) # most accurate: mtry 11 at 78.30%
+plot(rf_gridsearch) # most accurate: mtry 22 at 74.52830%
 
-# Now begin the full Random Forest analyses: going to use mtry = 9.695 (default), and mtry = 7; 7  was given from grid search tuning with decently high accuracy.
+# Now begin the full Random Forest analyses: going to use mtry = 9.695 (default), and mtry = 22; 22  was given from grid search tuning with decently high accuracy.
 
 # run a large number of trees with the above mtry values, in order to know what value of ntree achieves convergence of importance values between forests.
 # As a starting point, grow 10,000 trees and increase if necessary.
 
 # forests with default mtry:
-rf_default_a <- randomForest(x = data[,2:96], y = as.factor(data$paps_texture), importance=TRUE ,proximity=TRUE, ntree=10000, strata=as.factor(data$paps_texture), sampsize=sample_size)
+rf_default_a <- randomForest(x = data[,2:96], y = as.factor(data$paps_smooth_regressed), importance=TRUE ,proximity=TRUE, ntree=10000, strata=as.factor(data$paps_smooth_regressed), sampsize=sample_size)
 
-rf_default_b <- randomForest(x = data[,2:96], y = as.factor(data$paps_texture), importance=TRUE ,proximity=TRUE, ntree=10000, strata=as.factor(data$paps_texture), sampsize=sample_size)
+rf_default_b <- randomForest(x = data[,2:96], y = as.factor(data$paps_smooth_regressed), importance=TRUE ,proximity=TRUE, ntree=10000, strata=as.factor(data$paps_smooth_regressed), sampsize=sample_size)
 
 #Check correlation of predictor importance values between forests 
 importance_rf_default_a <- data.frame(importance(rf_default_a,type=1)) #type=1 is mean decrease in accuracy for classification, so a large, positive value means that permuting the variable led to a big decrease in prediction accuracy (which is indicative of an important predictor)
@@ -123,25 +123,26 @@ colnames(importance_rf_default_a)<-c("importance")
 importance_rf_default_b <- data.frame(importance(rf_default_b,type=1))
 colnames(importance_rf_default_b)<-c("importance")
 
-cor(importance_rf_default_a,importance_rf_default_b) # A correlation of 0.9966458 for predictor importance values between forests when mtry = 9.695 and ntree = 10,000
+cor(importance_rf_default_a,importance_rf_default_b) # A correlation of 0.997091 for predictor importance values between forests when mtry = 9.695 and ntree = 10,000
 
-# forests with mtry = 11
+# forests with mtry = 22
 
-rf_11_a <- randomForest(x = data[,2:96], y = as.factor(data$paps_texture), importance=TRUE ,proximity=TRUE, mtry=11, ntree=10000, strata=as.factor(data$paps_texture), sampsize=sample_size)
+rf_22_a <- randomForest(x = data[,2:96], y = as.factor(data$paps_smooth_regressed), importance=TRUE ,proximity=TRUE, mtry=11, ntree=10000, strata=as.factor(data$paps_smooth_regressed), sampsize=sample_size)
 
-rf_11_b <- randomForest(x = data[,2:96], y = as.factor(data$paps_texture), importance=TRUE ,proximity=TRUE, mtry=11, ntree=10000, strata=as.factor(data$paps_texture), sampsize=sample_size)
+rf_22_b <- randomForest(x = data[,2:96], y = as.factor(data$paps_smooth_regressed), importance=TRUE ,proximity=TRUE, mtry=11, ntree=10000, strata=as.factor(data$paps_smooth_regressed), sampsize=sample_size)
 
 #Check correlation of locus importance values between forests 
-importance_rf_11_a <- data.frame(importance(rf_11_a,type=1)) #type=1 is mean decrease in accuracy for classification, so a large, positive value means that permuting the variable led to a big decrease in prediction accuracy (which is indicative of an important locus)
-colnames(importance_rf_11_a)<-c("importance")
+importance_rf_22_a <- data.frame(importance(rf_22_a,type=1)) #type=1 is mean decrease in accuracy for classification, so a large, positive value means that permuting the variable led to a big decrease in prediction accuracy (which is indicative of an important locus)
+colnames(importance_rf_22_a)<-c("importance")
 
-importance_rf_11_b <- data.frame(importance(rf_11_b,type=1))
-colnames(importance_rf_11_b)<-c("importance")
+importance_rf_22_b <- data.frame(importance(rf_22_b,type=1))
+colnames(importance_rf_22_b)<-c("importance")
 
-cor(importance_rf_11_a,importance_rf_11_b) # A correlation of 0.9972759 for predictor importance values between forests when mtry = 11 and ntree = 10,000
+cor(importance_rf_22_a,importance_rf_22_b) # A correlation of 0.9974304 for predictor importance values between forests when mtry = 28 and ntree = 10,000
 
-# Build final model with mtry = 11, ntree = 10,000, as mtry = 11 had slightly importance value correlation
+# mtry = 22 had a slightly higher importance value correlation, so run both mtry = 22 and mtry = default
 
+# Build final model with mtry = 22, ntree = 10,000. 
 # Separate into training and test data (70% and 30%, respectively)
 set.seed(3)
 train = sample(1:nrow(data), 0.7*nrow(data))
@@ -151,27 +152,27 @@ data.train = data[-train,]
 # Create random forest model using training data set
 # Make sure to exclude the sample ID column (data.train[,-1])
 set.seed(3)
-data.model = randomForest(paps_texture ~ .,
+data.model = randomForest(paps_smooth_regressed ~ .,
                           data = data.train[,-1],
                           importance=TRUE,
                           proximity=TRUE,
-                          mtry=11, # based on tuning done above
+                          mtry=22, # based on tuning done above
                           ntree=10000, # based on tuning for ntree above
-                          strata=as.factor(data$paps_texture), sampsize=sample_size) # take into account imbalance in texture type
-data.model # OOB-ER = 25%
+                          strata=as.factor(data$paps_smooth_regressed), sampsize=sample_size) # take into account imbalance in texture type
+data.model # OOB-ER = 31.25%
 
 # Use the model built above to make predictions on the test data
 data.predict = predict(data.model,newdata = data.test[,-1])
 
 # Build a confusion matrix and calculate accuracy of predicted model
-table(Prediction = data.predict,Truth = data.test$paps_texture)
-sum(diag(table(Prediction = data.predict,Truth = data.test$paps_texture))) / sum(table(Prediction = data.predict,Truth = data.test$paps_texture)) # Accuracy of model with all variables: 71.62162%
+table(Prediction = data.predict,Truth = data.test$paps_smooth_regressed)
+sum(diag(table(Prediction = data.predict,Truth = data.test$paps_smooth_regressed))) / sum(table(Prediction = data.predict,Truth = data.test$paps_smooth_regressed)) # Accuracy of model with all variables: 63.51351%
 
 # Importance Sampling
 importance(data.model)
 varImpPlot(data.model, main = "Cm tumor smoothness")
 
-# Let's also build a model based on the default mtry, since it did decently and is very close to the tuned mtry
+# Build a model based on mtry = default and ntree = 10,0000
 
 # Separate into training and test data (70% and 30%, respectively)
 set.seed(3)
@@ -182,19 +183,20 @@ data.train = data[-train,]
 # Create random forest model using training data set
 # Make sure to exclude the sample ID column (data.train[,-1])
 set.seed(3)
-data.model_defaultmtry <- randomForest(paps_texture ~ .,
+data.model_defaultmtry <- randomForest(paps_smooth_regressed ~ .,
                                        data = data.train[,-1],
                                        importance=TRUE,
                                        proximity=TRUE,
                                        ntree=10000, # based on tuning for ntree above
-                                       strata=as.factor(data$paps_texture), sampsize=sample_size) # take into account imbalance in FP postiives vs FP negatives
-data.model_defaultmtry # OOB-ER = 28.12%
+                                       strata=as.factor(data$paps_smooth_regressed), sampsize=sample_size) # take into account imbalance in FP postiives vs FP negatives
+data.model_defaultmtry # OOB-ER = 34.38%
 
 # Use the model built above to make predictions on the test data
 data.predict_defaultmtry = predict(data.model_defaultmtry, newdata = data.test[,-1])
 
 # Build a confusion matrix and calculate accuracy of predicted model
-table(Prediction = data.predict_defaultmtry, Truth = data.test$paps_texture)
-sum(diag(table(Prediction = data.predict_defaultmtry,Truth = data.test$paps_texture))) / sum(table(Prediction = data.predict_defaultmtry,Truth = data.test$paps_texture)) # Accuracy of model with all variables: #74.32%
+table(Prediction = data.predict_defaultmtry, Truth = data.test$paps_smooth_regressed)
+sum(diag(table(Prediction = data.predict_defaultmtry,Truth = data.test$paps_smooth_regressed))) / sum(table(Prediction = data.predict_defaultmtry,Truth = data.test$paps_smooth_regressed)) # Accuracy of model with all variables: #66.21622%
 
-# RF built with mtry = 11 has better accuracy and OOB-ER than RF built with default mtry
+importance(data.model_defaultmtry)
+varImpPlot(data.model_defaultmtry, main = "Cm tumor smoothness default mtry")
